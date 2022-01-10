b31ec7542c6dbd1d260b5f9594a35b001238af46
Dred2048 <singhishu14@gmail.com>
2022-01-10 14:39:08 +0530

    Add project files.

---
+++ Bot.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DSharpPlus;
+using DSharpPlus.EventArgs;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Net;
+using DSharpPlus.Entities;
+using Newtonsoft.Json;
+
+namespace Discord_Bot
+{
+    public class Bot
+    {
+        public DiscordClient Client { get; private set; }
+        public CommandsNextConfiguration? commands { get; private set; }
+        public async Task RunAsynk()
+        {
+            var json = string.Empty;
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = await sr.ReadToEndAsync().ConfigureAwait(false);
+
+            var ConfigJson = JsonConvert.DeserializeObject<ConfigJson>(json);
+
+            var config = new DiscordConfiguration
+            {
+                Token = ConfigJson.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+                MinimumLogLevel = LogLevel.Debug, 
+                
+            };
+
+            Client = new DiscordClient(config);
+            Client.Ready += OnClientReady;
+
+            var commandsConfig = new CommandsNextConfiguration
+            {
+                StringPrefixes = new string [] { ConfigJson.Prefix},
+                EnableMentionPrefix = true,
+                EnableDms = false,
+            };
+
+            commands = new CommandsNextConfiguration(commandsConfig);
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private Task Client_Ready(DiscordClient sender, ReadyEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Task OnClientReady(DiscordClient client,ReadyEventArgs e)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
